4a6e24b61ef54e84a9ad0b7e2e5e1f5400d71e5f
FritoLay23 <alexanderlop790@gmail.com>
2023-04-29 09:28:43 -0400

    first commit on test

--- src/components/Footer.c.tsx
+++ src/components/Footer.c.tsx
@@ -5,12 +5,15 @@ function Footer() {
         <div className="contenedor_footer">
           <h3 className="footer_h3">SFJ</h3>
           <p className="footer_descripcion">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem a optio voluptatibus aut. Saepe, nostrum inventore! Suscipit dolorem numquam obcaecati, beatae quam aut ullam deleniti laudantium ratione est? Ab, beatae!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
+            a optio voluptatibus aut. Saepe, nostrum inventore! Suscipit dolorem
+            numquam obcaecati, beatae quam aut ullam deleniti laudantium ratione
+            est? Ab, beatae!
           </p>
         </div>
         <div className="footer_bajo">
           <p>
-            copyrigth &copy; 2023 SFJ. designed by{' '}
+            copyrigth &copy; 2023 SFJ. designed by{" Beiguete "}
             <span>Team</span>
           </p>
         </div>
