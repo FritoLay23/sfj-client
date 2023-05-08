94ae211a1d93b207c5f0adbbae5e91a2ec789bcd
Nestor <nestorvaldez@gmail.com>
2023-05-07 20:03:16 -0400

    datagrid

--- src/components/GamesM.c.tsx
+++ src/components/GamesM.c.tsx
@@ -1,5 +1,25 @@
+import Udatagrid from "./Udatagrid.c";
+
+
 const GamesM = ({ value }: any) => {
-  return <div>Ver</div>;
+
+  const GridProps = {
+    columns: [
+      { field: 'id', headerName: 'ID', width: 90 },
+      { field: 'type', headerName: 'Type', width: 150 },
+      { field: 'branch', headerName: 'Branch', width: 150 },
+      { field: 'title', headerName: 'Title', width: 150 },
+      { field: 'description', headerName: 'Description', width: 150 },
+      { field: 'questions', headerName: 'Questions', width: 150 },
+      { field: 'created_at', headerName: 'Created At', width: 150 },
+      { field: 'updated_at', headerName: 'Updated At', width: 150 },
+    ],
+    rows: [],
+  };
+
+  return (
+    <Udatagrid data={GridProps}/>
+  );
 };
 
 export default GamesM;
